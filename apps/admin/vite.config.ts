45e111c9616959d1cf9dc58e2d27ec7a5fc7a576
Carmel fred PATCHELLI LOUBENDJE LOUBAT <fred.patchelli@anac-gabon.lan>
2026-05-06 11:01:29 +0100

    fix: host issue

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,13 +1,18 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: ["safeline", "100.110.227.69", "127.0.0.1"],
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
